e1f06db433a118c1350f7b8956601313b326068e
Eric Xue <widefin@gmail.com>
2022-09-19 12:02:17 -0400

    EZ

---
+++ APTriangle.java
@@ -0,0 +1,72 @@
+public class APTriangle {
+    private double a, b, c;
+    private boolean exists;
+    
+    public APTriangle(double a, double b, double c){
+        int s[3] = { a, b, c };
+        Arrays.sort(s);
+        if (a+b>c && a+c>b && b+c>a){
+            exists = true;
+            this.a = s[0];
+            this.b = s[1];
+            this.c = s[2];
+        } else {
+            exists = false;
+            this.a = this.b = this.c = 0;
+        }
+    }
+    
+    public double getA(){
+        return a;
+    }
+    
+    public double getB(){
+        return b;
+    }
+    
+    public double getC(){
+        return c;
+    }
+    
+    public double getPerimeter(){
+        return a+b+c;
+    }
+    
+    public double getArea(){
+        double s = (a+b+c)/2;
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    }
+    
+    public String compareBySides(){
+        if (!exists) return null;
+        
+        if (a == b && b == c) return "Equilateral";
+        else if (a == b || b == c || c == a) return "Isosceles";
+        else return "Scalene";
+    }
+    
+    public String compareByAngles(){
+        if (!exists) return null;
+        int cs=c*c, as=a*a, bs=b*b;
+        
+        if (cs<as+bs) return "acute";
+        else if (cs==as+bs) return "right";
+        else  return "obtuse";
+    }
+    
+    public double largestAngle(){
+        if (!exists) return 0;
+        return Math.toDegrees(Math.acos((a*a + b*b - c*c)/(2*a*b)));
+    }
+    
+    public String locationOfCircumcenter(){
+        if (!exists) return null;
+        String ang = compareByAngles();
+        
+        if (ang == "obtuse") return "outside";
+        else if (ang == "right") return "on";
+        else return "inside";
+    }
+}
+    
+    
