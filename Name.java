918861ede34b71e86ee4e6f32e73e5892ccc810f
Eric Xue <widefin@gmail.com>
2022-09-19 14:37:10 -0400

    EASY

--- Name.java
+++ Name.java
@@ -27,16 +27,24 @@ public class Name {
         return f + " " + m + " " + l;
     }
     
+    public String lastFirstMiddle(){
+        return l + ", " + f + " " + m;
+    }
+    
+    public int length(){
+        return (f+m+l).replace(" ", "").length();
+    }
+    
     public boolean equals(Name a){
         return f.equalsIgnoreCase(a.getFirst()) && m.equalsIgnoreCase(a.getMiddle()) && l.equalsIgnoreCase(a.getLast());
     }
     
     public String initials(){
-        char a='', b='', c='';
-        if (f.length() > 0) a = Character.toUpperCase(f.charAt(0));
-        if (m.length() > 0) b = Character.toUpperCase(m.charAt(0));
-        if (l.length() > 0) c = Character.toUpperCase(l.charAt(0));
-        return "" + a + b + c;
+        String ini = "";
+        if (f.length() > 0) ini += Character.toUpperCase(f.charAt(0));
+        if (m.length() > 0) ini += Character.toUpperCase(m.charAt(0));
+        if (l.length() > 0) ini += Character.toUpperCase(l.charAt(0));
+        return ini;
     }
 }
     
