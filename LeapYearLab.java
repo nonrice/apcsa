3d734a48d6c2a3c7bb74d3a736de1c800fa9f3fd
Eric Xue <widefin@gmail.com>
2022-09-21 12:15:41 -0400

    VERY ANOYING

--- LeapYearLab.java
+++ LeapYearLab.java
@@ -1,37 +1,98 @@
-import java.util.Scanner;
-
-public class LeapYearLab {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int year;
-        do {
-            System.out.print("Enter a year 1582 or greater (0 to quit): ");
-            year = in.nextInt();
-            if (year == 0) break;
-
-            if (validity(year)){
-                if (leap(year)){
-                    System.out.println("\n" + year + " is a leap year");
-                } else {
-                    System.out.println("\n" + year + " is not a leap year");
-                }
-            } else {
-                System.out.println("\nERROR: the year must be greater than 1581");
-            }
-        } while (year != 0);
-
-        System.out.println("\nGood-bye");
-        in.close();
+public class SimpleDate {
+    private int d=1, m=1, y=1;
+    private int MSZ[] = { -1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    private static final String MNM[] = {
+        "",
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "November",
+        "December"
+    };
+    
+    public SimpleDate(int y){
+        this.y = y;
+        repairVars();
     }
-
-    public static boolean validity(int year){
-        return year >= 1582;
+    
+    public SimpleDate(int m, int y){
+        this.m = m;
+        this.y = y;
+        repairVars();
     }
-
-    public static boolean leap(int year){
-        if (year%4 == 0){
-            if (year%100 == 0){
-                return year%400 == 0;
+    
+    public SimpleDate(int d, int m, int y){
+        this.d = d;
+        this.m = m;
+        this.y = y;
+        repairVars();
+    }
+    
+    private void repairVars(){
+        if (isLeapYear()) MSZ[2] = 29;
+        if (m < 1 || m > 12) m = 1;
+        if (d < 1 || d > MSZ[m]) d = 1;
+    }
+    
+    public void advanceDay(){
+        if (++d > MSZ[m]){
+            d -= MSZ[m];
+            advanceMonth();
+        }
+    }
+        
+    public void advanceMonth(){
+        if (++m > 12){
+            m = 1;
+            advanceYear();
+        }
+        
+        d = Math.min(d, MSZ[m]);
+    }
+    
+    public void advanceYear(){
+        ++y;
+        if (isLeapYear()) MSZ[2] = 29;
+        else {
+            MSZ[2] = 28;
+            if (d == 29) d = 28;
+        }
+    }
+    
+    public int daysInMonth(){
+        return MSZ[m];
+    }
+    
+    public int getDay(){
+        return d;
+    }
+    
+    public String getLongDate(){
+        return MNM[m] + " " + d + ", " + y;
+    }
+    
+    public int getMonth(){
+        return m;
+    }
+    
+    public String getShortDate(){
+        return m + "/" + d + "/" + y;
+    }
+    
+    public int getYear(){
+        return y;
+    }
+    
+    public boolean isLeapYear(){
+        if (y%4 == 0){
+            if (y%100 == 0){
+                return y%400 == 0;
             }
             return true;
         }
