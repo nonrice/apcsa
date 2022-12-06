d5ce3147a88004719e828a392a94dee43a58fdd6
Eric Xue <widefin@gmail.com>
2022-12-06 12:16:48 -0500

    gay asf

--- unit5/movies/Subscriber.java
+++ unit5/movies/Subscriber.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Subscriber {
     private ArrayList<NewMovie> a;
     
@@ -14,10 +16,10 @@ public class Subscriber {
     }
     
     public double[] getFitCoefficients(){
-        double b = new double[a.size()];
-        for (int i=0; i<p.length; ++i){
-            for (int j=0; j<p.length; ++j){
-                if (j != i) b[i] += a[i].likenessScore(a[j]);
+        double b[] = new double[a.size()];
+        for (int i=0; i<b.length; ++i){
+            for (int j=0; j<b.length; ++j){
+                if (j != i) b[i] += a.get(i).likenessScore(a.get(j));
             }
             b[i] /= b.length-1;
         }
@@ -25,17 +27,20 @@ public class Subscriber {
     }
     
     public void removeOutliers(){
-        double b = getFitCoefficients();
+        double b[] = getFitCoefficients();
         double t = 0;
         for (int i=0; i<b.length; ++i) t += b[i];
         t /= b.length;
         t /= 2;
         
-        for (int i=0; i<a.length; ++i){
-            if (b[i] < t){
+        int j=0;
+        for (int i=0; i<a.size(); ++i){
+            if (b[j] < t){
                 a.remove(i);
+                System.out.println("removed");
                 --i;
             }
+            ++j;
         }
     }
 }
