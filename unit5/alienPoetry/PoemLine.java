a89ac3deff8f40a1acc34980212be3ee6a735cda
Eric Xue <widefin@gmail.com>
2022-12-08 12:03:46 -0500

    so ez

---
+++ unit5/alienPoetry/PoemLine.java
@@ -0,0 +1,12 @@
+public class PoemLine {
+    private String s;
+    
+    public PoemLine(String s){
+        this.s = s;
+    }
+    
+    public String getEnglishLine(){
+        return s;
+    }
+}
+    
