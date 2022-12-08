855fdeced3e98f138af1d0145af4bf3c9679ece3
Eric Xue <widefin@gmail.com>
2022-12-08 12:03:18 -0500

    The tester is written incorrectly...
    I really want to say something, but this is public, so I will refrain.

---
+++ unit5/alienPoetry/PoemStanza.java
@@ -0,0 +1,28 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PoemStanza {
+    private ArrayList<PoemLine> a;
+    
+    public PoemStanza(ArrayList<PoemLine> list){
+        a = list;
+    }
+    
+    public String convertToAlienLine(String s){
+        String[] w = s.split(" ");
+        Collections.reverse(Arrays.asList(w));
+        return String.join(" ", w);
+    }
+    
+    public ArrayList<String> convertToAlienStanza(){
+        String r[] = new String[a.size()];
+        
+        for (int i=0; i<a.size(); ++i) r[i] = convertToAlienLine(a.get(i).getEnglishLine());
+        ArrayList<String> rt = new ArrayList<String>(Arrays.asList(r));
+        Collections.reverse(rt);
+        return rt;
+    }
+}
+        
+     
