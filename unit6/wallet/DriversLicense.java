d0e638e0605639f6853a5dd0ceac16de172627b8
Eric Xue <widefin@gmail.com>
2023-01-17 12:56:54 -0500

    Create DriversLicense.java

---
+++ unit6/wallet/DriversLicense.java
@@ -0,0 +1,17 @@
+public class DriversLicense extends Card {
+    private int id, exp;
+    
+    DriversLicense(String name, int id, int exp){
+        super(name);
+        this.id = id;
+        this.exp = exp;
+    }
+    
+    public void printCard(){
+        System.out.println("Card holder: " + name + "\nLicense # " + id + " expires in " + exp);
+    }
+    
+    public boolean isExpired(){
+        return exp < 2023;
+    }
+}
