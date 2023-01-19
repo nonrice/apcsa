6a6d0237bbfc890d8d88993606696ead9b8e57e9
Eric Xue <widefin@gmail.com>
2023-01-19 12:19:18 -0500

    Create MovieTriology.java

---
+++ unit6/movie/MovieTriology.java
@@ -0,0 +1,18 @@
+public class MovieTrilogy extends Movie {
+	private String genre;
+	
+	MovieTrilogy(Movie a, Movie b, Movie c){
+		super("", 0);
+		score = (a.getScore() + b.getScore() + c.getScore())/3;
+		title = a.getTitle() + "/" + b.getTitle() + "/" + c.getTitle();
+		if (a.getGenre().equals(b.getGenre()) && b.getGenre().equals(c.getGenre())){
+			genre = a.getGenre();
+		}
+	}
+	
+	public String getGenre(){
+		String ans = "Trilogy";
+		if (genre != null) ans += "(" + genre + ")";
+		return ans;
+	}
+}
